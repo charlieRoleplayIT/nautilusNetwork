a509e24841e047bd309fc720112d8fcb0e17045a
charlieRoleplayIT <pantomino7@gmail.com>
2025-05-23 16:01:22 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const navbar = document.querySelector(".navbar");
+const navbarLinks = document.querySelector(".links");
+const hamburger = document.querySelector(".hamburger");
+
+hamburger.addEventListener("click", () => {
+    navbar.classList.toggle("active");
+    navbarLinks.classList.toggle("active");
+});
+
+document.addEventListener("click", (event) => {
+    if (!navbar.contains(event.target)) {
+        navbar.classList.remove("active");
+        navbarLinks.classList.remove("active");
+    }
+});
+
+const accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
+
+accordionItemHeaders.forEach(accordionItemHeader => {
+    accordionItemHeader.addEventListener("click", () => {
+        accordionItemHeader.classList.toggle("active");
+        const accordionItemBody = accordionItemHeader.nextElementSibling;
+
+        if (accordionItemHeader.classList.contains("active")) {
+            accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+        } else {
+            accordionItemBody.style.maxHeight = "0px";
+        }
+    });
+});
